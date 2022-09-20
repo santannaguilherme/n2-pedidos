43ed13230618717d0d4433266a8aff2d6088d8d1
Guilherme Sant'Anna <guilherme.sanntana@gmail.com>
2022-09-20 20:34:04 -0300

    Filtro

--- pages/home/index.js
+++ pages/home/index.js
@@ -3,14 +3,26 @@ import { View } from "react-native";
 import Product from "../../components/product";
 import { getAllProducts } from "../register/product/service/dbService";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import styles from "./style";
+import { Dropdown } from "react-native-element-dropdown";
+import { getAll } from "../register/category/service/dbService";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
   const [purchases, setPurchases] = useState([]);
 
+
+  const [categories, setCategories] = useState([]);
   async function tableUseEffect() {
     const data = await getAllProducts();
     setProducts(data);
+    setFilteredProducts(data)
+    const cat = await getAll();
+    const select = cat.map((c) => {
+      return { label: c.value, value: c.value };
+    });
+    setCategories(select);
   }
   useEffect(() => {
     tableUseEffect();
@@ -32,9 +44,28 @@ export default function Home() {
     await AsyncStorage.setItem("@purchases", jsonValue);
 
   };
+
+  function filter(category){
+    const prod = products.filter(pro => (pro.category ===category))
+    console.log(prod)
+    setFilteredProducts(prod)
+  }
   return (
     <View>
-      {products.map((p, index) => (
+      <Dropdown
+          // style={[styles.dropdown]}
+          // placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          data={categories}
+          maxHeight={300}
+          labelField="label"
+          valueField="value"
+          placeholder={""}
+          onChange={(item) => {
+           filter(item.value);
+          }}
+        />
+      {filteredProducts.map((p, index) => (
         <Product
           key={index.toString()}
           product={p}
